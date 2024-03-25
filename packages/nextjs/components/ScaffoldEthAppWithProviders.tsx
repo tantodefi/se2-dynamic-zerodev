7ebf116660e3a7e2f58240ed9c33d242c5b92a49
tantodefi <tantodefi@protonmail.com>
2024-03-25 09:34:18 -0600

    added dynamic and zerodev for social + email login with embedded AA wallets

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { ZeroDevSmartWalletConnectors } from "@dynamic-labs/ethereum-aa";
+import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import { useTheme } from "next-themes";
 import { Toaster } from "react-hot-toast";
-import { WagmiConfig } from "wagmi";
+// import { WagmiConfig } from "wagmi";
 import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { ProgressBar } from "~~/components/scaffold-eth/ProgressBar";
 import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
 import { useGlobalState } from "~~/services/store/store";
-import { wagmiConfig } from "~~/services/web3/wagmiConfig";
+// import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 import { appChains } from "~~/services/web3/wagmiConnectors";
 
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
@@ -46,15 +50,33 @@ export const ScaffoldEthAppWithProviders = ({ children }: { children: React.Reac
   }, []);
 
   return (
-    <WagmiConfig config={wagmiConfig}>
+    <DynamicContextProvider
+      settings={{
+        environmentId: "1d92c1b4-8263-4f72-9449-946d57c21d74",
+        walletConnectors: [EthereumWalletConnectors, ZeroDevSmartWalletConnectors],
+      }}
+    >
       <ProgressBar />
-      <RainbowKitProvider
-        chains={appChains.chains}
-        avatar={BlockieAvatar}
-        theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
-      >
-        <ScaffoldEthApp>{children}</ScaffoldEthApp>
-      </RainbowKitProvider>
-    </WagmiConfig>
+      <DynamicWagmiConnector>
+        <RainbowKitProvider
+          chains={appChains.chains}
+          avatar={BlockieAvatar}
+          theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
+        >
+          <ScaffoldEthApp>{children}</ScaffoldEthApp>
+        </RainbowKitProvider>
+      </DynamicWagmiConnector>
+    </DynamicContextProvider>
+
+    // <WagmiConfig config={wagmiConfig}>
+    //   <ProgressBar />
+    //   <RainbowKitProvider
+    //     chains={appChains.chains}
+    //     avatar={BlockieAvatar}
+    //     theme={mounted ? (isDarkMode ? darkTheme() : lightTheme()) : lightTheme()}
+    //   >
+    //     <ScaffoldEthApp>{children}</ScaffoldEthApp>
+    //   </RainbowKitProvider>
+    // </WagmiConfig>
   );
 };
